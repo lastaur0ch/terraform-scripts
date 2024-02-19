f0b7e529a9aa2ef118b89750caad812b0d316d05
lastaur0ch <joelxjohnson@gmail.com>
2024-02-19 05:29:00 +0100

    resource name updated

--- 1-test_connection/main.tf
+++ 1-test_connection/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "boilerplate" {
+resource "aws_instance" "test_connection" {
   ami = "ami-0c7217cdde317cfec"
   instance_type = "t2.micro"
   subnet_id = "subnet"
